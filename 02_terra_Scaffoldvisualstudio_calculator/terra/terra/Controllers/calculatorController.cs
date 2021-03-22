28506de0b06e81c03ef314610f24dde4e7ef48d8
Cristianoterra <cristianoterr@gmail.com>
2021-03-22 11:54:47 -0300

    your_message

---
+++ 02_terra_Scaffoldvisualstudio_calculator/terra/terra/Controllers/calculatorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace terra.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class calculatorController : ControllerBase
+    {
+
+        private readonly ILogger<calculatorController> _logger;
+        private bool isnumber;
+
+        public calculatorController(ILogger<calculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string firstnumber, string secondnumber)
+        {
+            if (isnumeric(firstnumber) && isnumeric(secondnumber)) 
+            {
+                var sum = converttodecimal(firstnumber) + converttodecimal(secondnumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private bool isnumeric(string strnumber)
+        {
+            Double number;
+            bool isnumber = double.TryParse(
+                strnumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number); 
+            return isnumber;
+        }
+
+        private decimal converttodecimal(string strnumber)
+        {
+            decimal decimalvalue;
+            if(decimal.TryParse(strnumber, out decimalvalue)) 
+            {
+                return decimalvalue;
+            }
+            return (0);
+        }
+    }
+}
