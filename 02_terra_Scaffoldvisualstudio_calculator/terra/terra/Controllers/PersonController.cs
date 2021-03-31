bfa6a1f706dc1619c13856aa0ae4bc866bfc3f3a
Cristianoterra <cristianoterr@gmail.com>
2021-03-31 09:36:47 -0300

    working with four verbs

--- 02_terra_Scaffoldvisualstudio_calculator/terra/terra/Controllers/PersonController.cs
+++ 02_terra_Scaffoldvisualstudio_calculator/terra/terra/Controllers/PersonController.cs
@@ -9,102 +9,21 @@ namespace terra.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class calculatorController : ControllerBase
+    public class personController : ControllerBase
     {
 
-        private readonly ILogger<calculatorController> _logger;
+        private readonly ILogger<personController> _logger;
         private bool isnumber;
 
-        public calculatorController(ILogger<calculatorController> logger)
+        public personController(ILogger<personController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet("sum/{firstnumber}/{secondnumber}")]
-        public IActionResult sum(string firstnumber, string secondnumber)
-        {
-            if (isnumeric(firstnumber) && isnumeric(secondnumber)) 
-            {
-                var sum = converttodecimal(firstnumber) + converttodecimal(secondnumber);
-                return Ok(sum.ToString());
-            }
-            return BadRequest("Invalid input");
-        }
-
         [HttpGet("subtraction/{firstnumber}/{secondnumber}")]
         public IActionResult subtraction(string firstnumber, string secondnumber)
-        {
-            if (isnumeric(firstnumber) && isnumeric(secondnumber))
-            {
-                var sum = converttodecimal(firstnumber) - converttodecimal(secondnumber);
-                return Ok(sum.ToString());
-            }
+        { 
             return BadRequest("Invalid input");
-        }
-
-        [HttpGet("mean/{firstnumber}/{secondnumber}")]
-        public IActionResult mean(string firstnumber, string secondnumber)
-        {
-            if (isnumeric(firstnumber) && isnumeric(secondnumber))
-            {
-                var sum = (converttodecimal(firstnumber) + converttodecimal(secondnumber)) / 2;
-                return Ok(sum.ToString());
-            }
-            return BadRequest("Invalid input");
-        }
-
-        [HttpGet("multiplication/{firstnumber}/{secondnumber}")]
-        public IActionResult multiplication(string firstnumber, string secondnumber)
-        {
-            if (isnumeric(firstnumber) && isnumeric(secondnumber))
-            {
-                var sum = converttodecimal(firstnumber) * converttodecimal(secondnumber);
-                return Ok(sum.ToString());
-            }
-            return BadRequest("Invalid input");
-        }
-
-        [HttpGet("division/{firstnumber}/{secondnumber}")]
-        public IActionResult division(string firstnumber, string secondnumber)
-        {
-            if (isnumeric(firstnumber) && isnumeric(secondnumber))
-            {
-                var sum = converttodecimal(firstnumber) / converttodecimal(secondnumber);
-                return Ok(sum.ToString());
-            }
-            return BadRequest("Invalid input");
-
-        }
-
-        [HttpGet("square-root/{firstnumber}")]
-        public IActionResult squareroot(string firstnumber)
-        {
-            if (isnumeric(firstnumber))
-            {
-                var squareroot = Math.Sqrt((double)converttodecimal(firstnumber));
-                return Ok(squareroot.ToString());
-            }
-            return BadRequest("Invalid input");
-        }
-        private bool isnumeric(string strnumber)
-        {
-            Double number;
-            bool isnumber = double.TryParse(
-                strnumber, 
-                System.Globalization.NumberStyles.Any, 
-                System.Globalization.NumberFormatInfo.InvariantInfo, 
-                out number); 
-            return isnumber;
-        }
-
-        private decimal converttodecimal(string strnumber)
-        {
-            decimal decimalvalue;
-            if(decimal.TryParse(strnumber, out decimalvalue)) 
-            {
-                return decimalvalue;
-            }
-            return (0);
         }
     }
 }
